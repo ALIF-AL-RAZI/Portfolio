ca45bc7f903a76030806a7f452eeb0082e31cc84
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-08-16 21:54:38 +0600

    second commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-jetbrains Mono'
+  });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header/>
+        {children}</body>
     </html>
   );
 }
