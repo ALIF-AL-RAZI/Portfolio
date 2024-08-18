5e25e32ba38009e538aa7c3ea8c381bd90290425
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-08-18 20:08:17 +0600

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header/>
-        {children}</body>
+        {children}
+        </body>
     </html>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
 const Header = () => {
-  return (
-    <header>Header</header>
-  )
-}
+return (
+<header className="py-8 xl:py-12 text-white">
+<div className="container mx-auto flex justify-between items-center">
+{/* Logo */}
+<Link href="/">
+<h1 className="text-4xl font-semibold">
+Alif <span className="text-accent">Al Razi</span>
+</h1>
+</Link>
+{/* desktop nav */}
 
-export default Header
+<div className="hidden xl:flex items-center gap-8">
+  <Nav />
+  <Link href="/contat">
+  <Button>Hire me</Button>
+  </Link>
+</div>
+
+<div className="xl:hidden"> mobile nav</div>
+
+</div>
+</header>
+);
+};
+export default Header;
