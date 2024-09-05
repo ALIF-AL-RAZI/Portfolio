bd54a163aa04c2b98513fc2dd0c3a78889d07a2a
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-09-05 14:33:58 +0600

    &apos; 2

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -46,7 +46,7 @@ return (
     <div className="flex flex-col xl:flex-row gap-[30px] ">
       <div className="xl:h-[54%] order-2 xl:order-none">
         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-          <h3 className="text-4xl text-accent">Let's work together</h3>
+          <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
           <p className="text-white/60">
             Hello, it&apos;s really a pain to be followed. Him
             All this should be borne by no wise man.
