2be5ef0a7f386c31a4c7abc292cb2bf590fe35f7
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-09-05 14:30:23 +0600

    &apos; solve

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,60 +35,51 @@ import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 const Contact = () => {
 return (
   <motion.section
-initial={{ opacity: 0 }}
-animate={{
-opacity: 1,
-transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-}}
-className="py-6"
->
-<div className="container mx-auto">
-<div className="flex flex-col xl:flex-row gap-[30px] ">
-<div className="xl:h-[54%] order-2 xl:order-none">
-<form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-<h3 className="text-4xl text-accent">Let's work together</h3>
-<p className="text-white/60">
-Hello, it's really a pain to be followed. Him
-All this should be borne by no wise man.
-</p>
-{/* input */}
-<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-<Input type="firstname" placeholder="Firstname" />
-<Input type="lastname" placeholder="Lastname" />
-<Input type="email" placeholder="Email address" />
-<Input type="phone" placeholder="Phone number" />
-</div>
-<Select>
+    initial={{ opacity: 0 }}
+    animate={{
+    opacity: 1,
+    transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+    }}
+    className="py-6"
+  > 
+  <div className="container mx-auto">
+    <div className="flex flex-col xl:flex-row gap-[30px] ">
+      <div className="xl:h-[54%] order-2 xl:order-none">
+        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+          <h3 className="text-4xl text-accent">Let's work together</h3>
+          <p className="text-white/60">
+            Hello, it&apos;s really a pain to be followed. Him
+            All this should be borne by no wise man.
+          </p>
+          {/* input */} 
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <Input type="firstname" placeholder="Firstname" />
+            <Input type="lastname" placeholder="Lastname" />
+            <Input type="email" placeholder="Email address" />
+            <Input type="phone" placeholder="Phone number" />
+          </div>
+          <Select>
+            <SelectTrigger className="w-full">
+            <SelectValue placeholder="Select a service" />
+            </SelectTrigger>
+            <SelectContent>
+            <SelectGroup>
+            <SelectLabel>Select a service</SelectLabel>
+            <SelectItem value="est">Web Development</SelectItem>
+            <SelectItem value="cst">UI/UX Design</SelectItem>
+            <SelectItem value="mst">Logo Design</SelectItem>
+            </SelectGroup>
+            </SelectContent>
+          </Select>
 
-<SelectTrigger className="w-full">
+          <Textarea
+            className="h-[200px]"
+            placeholder="Type your message here..."
+          />
 
-<SelectValue placeholder="Select a service" />
-
-</SelectTrigger>
-
-<SelectContent>
-
-<SelectGroup>
-
-<SelectLabel>Select a service</SelectLabel>
-
-<SelectItem value="est">Web Development</SelectItem>
-
-<SelectItem value="cst">UI/UX Design</SelectItem>
-
-<SelectItem value="mst">Logo Design</SelectItem>
-
-</SelectGroup>
-
-</SelectContent>
-
-</Select>
-<Textarea
-className="h-[200px]"
-placeholder="Type your message here..."/>
-<Button size="md" className="max-w-40">Send message</Button>
+          <Button size="md" className="max-w-40">Send message</Button>
   
-</form>
+        </form>
 
 </div>
 <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
